10e965d76c488cd2ada6d00e6213bbf09def2849
Alexandr Gritsunov <ickpimo@gmail.com>
2024-05-10 22:50:50 +0300

    Добавлены права доступа для контроллеров

--- provider/views.py
+++ provider/views.py
@@ -3,6 +3,7 @@ from django.contrib.contenttypes.models import ContentType
 from rest_framework import generics, serializers
 from django_filters.rest_framework import DjangoFilterBackend
 from provider.models import Factory, Retail, Entrepreneur
+from provider.permissions import IsActiveUser
 from provider.serializers import FactorySerializer, RetailSerializer, EntrepreneurSerializer
 
 
@@ -11,6 +12,7 @@ class RetailListCreateAPIView(generics.ListCreateAPIView):
     queryset = Retail.objects.all()
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ('contact__country', )
+    permission_classes = [IsActiveUser]
 
     def perform_create(self, serializer):
         supplier_id = serializer.validated_data.get('supplier_id')
@@ -29,36 +31,27 @@ class RetailListCreateAPIView(generics.ListCreateAPIView):
 class RetailRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = RetailSerializer
     queryset = Retail.objects.all()
+    permission_classes = [IsActiveUser]
 
 
 class EntrepreneurListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = EntrepreneurSerializer
     queryset = Entrepreneur.objects.all()
 
-    def perform_create(self, serializer):
-        supplier_id = serializer.validated_data.get('supplier_id')
-        supplier_type = serializer.validated_data.get('supplier_type')
-
-        if supplier_id and supplier_type:
-            content_type = ContentType.objects.get_for_id(supplier_type.id)
-            model_class = apps.get_model(content_type.app_label, content_type.model)
-            try:
-                model_instance = model_class.objects.get(pk=supplier_id)
-            except model_class.DoesNotExist:
-                raise serializers.ValidationError({'supplier_id': 'Неверный идентификатор поставщика'})
-            serializer.save()
-
 
 class EntrepreneurRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = EntrepreneurSerializer
     queryset = Entrepreneur.objects.all()
+    permission_classes = [IsActiveUser]
 
 
 class FactoryListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = FactorySerializer
     queryset = Factory.objects.all()
+    permission_classes = [IsActiveUser]
 
 
 class FactoryRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = FactorySerializer
     queryset = Factory.objects.all()
+    permission_classes = [IsActiveUser]
